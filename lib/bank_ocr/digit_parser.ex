b772def957ea51b76382d418800bc074d3222dc8
Jonathan Mason <jonathan.e.mason@gmail.com>
2018-03-06 09:37:56 -0800

    Naively parse all ten digits.

--- lib/bank_ocr/digit_parser.ex
+++ lib/bank_ocr/digit_parser.ex
@@ -5,5 +5,13 @@ defmodule BankOcr.DigitParser do
 
   def parse_digit(["   ", "  |", "  |"]), do: 1
   def parse_digit([" _ ", " _|", "|_ "]), do: 2
+  def parse_digit([" _ ", " _|", " _|"]), do: 3
+  def parse_digit(["   ", "|_|", "  |"]), do: 4
+  def parse_digit([" _ ", "|_ ", " _|"]), do: 5
+  def parse_digit([" _ ", "|_ ", "|_|"]), do: 6
+  def parse_digit([" _ ", "  |", "  |"]), do: 7
+  def parse_digit([" _ ", "|_|", "|_|"]), do: 8
+  def parse_digit([" _ ", "|_|", " _|"]), do: 9
+  def parse_digit([" _ ", "| |", "|_|"]), do: 0
 end
 
--- test/bank_ocr/digit_parser_test.exs
+++ test/bank_ocr/digit_parser_test.exs
@@ -3,19 +3,73 @@ defmodule BankOcr.DigitParserTest do
 
   import BankOcr.DigitParser
 
-  test "parses a 1" do
-    one = ["   ",
-           "  |",
-           "  |"]
-    
-    assert parse_digit(one) == 1
+  test "parses 1" do
+    digit = ["   ",
+             "  |",
+             "  |"]
+    assert parse_digit(digit) == 1
   end
 
-  test "parses a 2" do
-    two = [" _ ",
-           " _|",
-           "|_ "]
-    
-    assert parse_digit(two) == 2
+  test "parses 2" do
+    digit = [" _ ",
+             " _|",
+             "|_ "]
+    assert parse_digit(digit) == 2
+  end
+
+  test "parses 3" do
+    digit = [" _ ",
+             " _|",
+             " _|"]
+    assert parse_digit(digit) == 3
+  end
+
+  test "parses 4" do
+    digit = ["   ",
+             "|_|",
+             "  |"]
+    assert parse_digit(digit) == 4
+  end
+
+  test "parses 5" do
+    digit = [" _ ",
+             "|_ ",
+             " _|"]
+    assert parse_digit(digit) == 5
+  end
+
+  test "parses 6" do
+    digit = [" _ ",
+             "|_ ",
+             "|_|"]
+    assert parse_digit(digit) == 6
+  end
+
+  test "parses 7" do
+    digit = [" _ ",
+             "  |",
+             "  |"]
+    assert parse_digit(digit) == 7
+  end
+
+  test "parses 8" do
+    digit = [" _ ",
+             "|_|",
+             "|_|"]
+    assert parse_digit(digit) == 8
+  end
+
+  test "parses 9" do
+    digit = [" _ ",
+             "|_|",
+             " _|"]
+    assert parse_digit(digit) == 9
+  end
+
+  test "parses 0" do
+    digit = [" _ ",
+             "| |",
+             "|_|"]
+    assert parse_digit(digit) == 0
   end
 end
